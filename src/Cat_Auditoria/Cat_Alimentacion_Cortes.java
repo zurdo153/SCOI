0af0b042ff93d2798fea69200b22273841cf8ee4
zurdo153 <zurdo153@hotmail.com>
2014-10-19 12:40:44 -0600

    bajar cambios de retiro cajero

--- src/Cat_Auditoria/Cat_Alimentacion_Cortes.java
+++ src/Cat_Auditoria/Cat_Alimentacion_Cortes.java
@@ -3336,7 +3336,9 @@ public class Cat_Alimentacion_Cortes extends JFrame{
 						calculoDinamico();
 						btnSalir.setEnabled(false);
 //						corte.actualizar(tabla_retiros());
-//						
+						
+//						REPORTE AQUI
+						
 						dispose();				
 				}
 			};
