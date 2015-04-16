80f4ddfb42c27c8b4ad9b770f83e0c76a711bd90
zurdo153 <zurdo153@hotmail.com>
2015-04-15 18:09:53 -0600

    cambios de reporte de ventas 15-04-2015

--- src/Cat_Reportes/Cat_Reportes_De_Ventas.java
+++ src/Cat_Reportes/Cat_Reportes_De_Ventas.java
@@ -207,6 +207,9 @@ public class Cat_Reportes_De_Ventas extends JFrame {
     
 	Border blackline, etched, raisedbevel, loweredbevel, empty;
 	
+	String parametroGeneral = "";
+	String Lista="";
+	
 	public Cat_Reportes_De_Ventas(String parametro, String operador){
 		cont.add(panel);
 		setSize(1024,768);
@@ -526,6 +529,7 @@ public void filtroProductos(String cadena){
             txtFiltroFamilia.setText("");
             txtFiltroLinea.setText("");
         	panelEnableTrue();
+        	Lista="";
 		}
 	};
 	
@@ -537,51 +541,128 @@ public void filtroProductos(String cadena){
             txtFiltroFamilia.setText("");
             txtFiltroLinea.setText("");
         	panelEnableTrue();
+        	Lista="";
 		}
 	};
 	
 	ActionListener limpiar_filtro_categorias = new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
-            txtFiltroCategoria.setText("");
             
-            	btnFiltroClase.setEnabled(true);
+			panelEnableFalse();
+			txtFiltroCategoria.setText("");
+			limpiar_vacios();
+//			 btnFiltroCategoria.setEnabled(true);
+//			 btnLimpiarFiltroCategoria.setEnabled(true);
+//			 
+//	         btnLimpiarFiltroClase.setEnabled(true);
+//	            
+//	            	if(txtFiltroClase.getText().equals("")){
+//	            		btnFiltroClase.setEnabled(true);
+//	            		
+//	            		if(txtFiltroClase.getText().equals("")){
+//		            		btnFiltroClase.setEnabled(true);
+//		            		btnLimpiarFiltroClase.setEnabled(true);
+//		            		
+//		            		btnFiltroProducto.setEnabled(true);
+//		            	}
+//	            	}
+            
 		}
 	};
 	
 	ActionListener limpiar_filtro_familias = new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
-            txtFiltroFamilia.setText("");
-            
-            btnFiltroCategoria.setEnabled(true);
-            
-            if(txtFiltroClase.getText().equals("")){
-            	btnFiltroClase.setEnabled(true);
-            }
+           
+			panelEnableFalse();
+			txtFiltroFamilia.setText("");
+			limpiar_vacios();
+//            btnFiltroFamilia.setEnabled(true);
+//            btnLimpiarFiltroFamilia.setEnabled(true);
+//            
+//            btnLimpiarFiltroCategoria.setEnabled(true);
+//            
+//            	if(txtFiltroCategoria.getText().equals("")){
+//            		btnFiltroCategoria.setEnabled(true);
+//            		
+//            		if(txtFiltroClase.getText().equals("")){
+//	            		btnFiltroClase.setEnabled(true);
+//	            		btnLimpiarFiltroClase.setEnabled(true);
+//	            		
+//	            		btnFiltroProducto.setEnabled(true);
+//	            	}
+//            	}
+            	
 		}
 	};
 	
 	ActionListener limpiar_filtro_lineas = new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
-            txtFiltroLinea.setText("");
-            
-            	btnFiltroFamilia.setEnabled(true);
-            	btnLimpiarFiltroFamilia.setEnabled(true);
-            	
-            	if(txtFiltroFamilia.getText().equals("")){
-            		if(txtFiltroCategoria.getText().equals("")){
-	                	btnFiltroCategoria.setEnabled(true);
-	                	btnLimpiarFiltroCategoria.setEnabled(true);
-	                	
-	                	if(txtFiltroClase.getText().equals("")){
-	                    	btnFiltroClase.setEnabled(true);
-	                    	btnLimpiarFiltroClase.setEnabled(true);
-	                    }
-	                }
-            	}
-            	
+			
+			panelEnableFalse();
+			txtFiltroLinea.setText("");
+			limpiar_vacios();
+//            
+//            btnFiltroLinea.setEnabled(true);
+//            btnLimpiarFiltroLinea.setEnabled(true);
+//            
+//            btnLimpiarFiltroFamilia.setEnabled(true);
+//            
+//            	if(txtFiltroFamilia.getText().equals("")){
+//            		btnFiltroFamilia.setEnabled(true);
+//            		
+//            		if(txtFiltroCategoria.getText().equals("")){
+//                		btnFiltroCategoria.setEnabled(true);
+//                		btnLimpiarFiltroCategoria.setEnabled(true);
+//                		
+//                		if(txtFiltroClase.getText().equals("")){
+//    	            		btnFiltroClase.setEnabled(true);
+//    	            		btnLimpiarFiltroClase.setEnabled(true);
+//    	            		
+//    	            		btnFiltroProducto.setEnabled(true);
+//    	            	}
+//                	}
+//            	}
             	
 		}
 	};
+	
+	public void limpiar_vacios(){
+		panelEnableFalse();
+		txtFiltroLinea.setText("");
+        
+        btnFiltroLinea.setEnabled(true);
+        btnLimpiarFiltroLinea.setEnabled(true);
+        
+        btnLimpiarFiltroFamilia.setEnabled(true);
+        
+        	if(txtFiltroFamilia.getText().equals("")){
+        		btnFiltroFamilia.setEnabled(true);
+        		
+        		if(txtFiltroCategoria.getText().equals("")){
+            		btnFiltroCategoria.setEnabled(true);
+            		btnLimpiarFiltroCategoria.setEnabled(true);
+            		
+            		if(txtFiltroClase.getText().equals("")){
+	            		btnFiltroClase.setEnabled(true);
+	            		btnLimpiarFiltroClase.setEnabled(true);
+	            		
+	            		btnFiltroProducto.setEnabled(true);
+	            	}
+            		btnLimpiarFiltroClase.setEnabled(true);
+            	}
+        		btnLimpiarFiltroCategoria.setEnabled(true);
+        	}
+        	btnLimpiarFiltroFamilia.setEnabled(true);
+        	
+        	
+        	cmbOperador_Productos.setSelectedIndex(0);
+        	cmbOperador_Clase.setSelectedIndex(0);
+        	cmbOperador_Categoria.setSelectedIndex(0);
+        	cmbOperador_Familia.setSelectedIndex(0);
+        	cmbOperador_Linea.setSelectedIndex(0);
+        	
+        	Lista="";
+	}
 	
 //	----------------------------------------------------------------------------------------------------------------------------------------
 	
@@ -662,7 +743,7 @@ public void filtroProductos(String cadena){
     	btnFiltroFamilia.setEnabled(false);
     	btnFiltroLinea.setEnabled(false);
     	
-    	btnLimpiarFiltroProducto.setEnabled(false);
+//    	btnLimpiarFiltroProducto.setEnabled(false);
     	btnLimpiarFiltroClase.setEnabled(false);
     	btnLimpiarFiltroCategoria.setEnabled(false);
     	btnLimpiarFiltroFamilia.setEnabled(false);
@@ -676,7 +757,7 @@ public void filtroProductos(String cadena){
     	btnFiltroFamilia.setEnabled(true);
     	btnFiltroLinea.setEnabled(true);
     	
-    	btnLimpiarFiltroProducto.setEnabled(true);
+//    	btnLimpiarFiltroProducto.setEnabled(true);
     	btnLimpiarFiltroClase.setEnabled(true);
     	btnLimpiarFiltroCategoria.setEnabled(true);
     	btnLimpiarFiltroFamilia.setEnabled(true);
@@ -856,7 +937,7 @@ public void filtroProductos(String cadena){
 						txtNombre_Completo.setText("");
 						
 						int contador=0;
-				 		String Lista="('";	
+				 		 Lista="('";	
 				 			for(int i=0; i<tablaFiltro.getRowCount(); i++){
 				 				if(Boolean.parseBoolean(modeloFiltro.getValueAt(i, 2).toString()) == true){
 				 					String posicion = modeloFiltro.getValueAt(i, 0).toString().trim();
@@ -885,42 +966,64 @@ public void filtroProductos(String cadena){
 				 		        
 				 		        	
 				 		            switch(Operador){
-				 			    		case "Igual"		:operador_simbolo=" = "; break;
-				 			    		case "Esta en lista":operador_simbolo=" in "; break;
-				 			    		case "Menor que"	:operador_simbolo=" < "; break;
-				 			    		case "Mayor que"	:operador_simbolo=" > "; break;
-				 			    		case "Diferente"	:operador_simbolo=" <> "; break;
+				 			    		case "Igual"		:operador_simbolo=" = "; 
+				 			    		panelEnableFalse();
+				 			    		parametroGeneral=Lista;
+				 			    		
+				 			    		break;
+				 			    		case "Esta en lista":operador_simbolo=" in "; 
+				 			    		panelEnableFalse();
+				 			    		
+				 			    		break;
+				 			    		case "Menor que"	:operador_simbolo=" < "; 
+				 			    		panelEnableFalse();
+				 			    		
+				 			    		break;
+				 			    		case "Mayor que"	:operador_simbolo=" > "; 
+				 			    		panelEnableFalse();
+				 			    		
+				 			    		break;
+				 			    		case "Diferente"	:operador_simbolo=" <> "; 
+				 			    		panelEnableFalse();
+				 			    		
+				 			    		break;
 				 		    		}
-				 				
+				 				Lista=operador_simbolo+Lista;
 				 				switch(folio_columna){
-							 				case "clase_producto":	txtFiltroClase.setText(operador_simbolo+Lista)		;
+				 				           
+							 				case "clase_producto":	txtFiltroClase.setText(Lista)		;
+							 										if(Operador.equals("Igual")||Operador.equals("Esta en lista")){
+							 											btnFiltroCategoria.setEnabled(true);
+							 											btnLimpiarFiltroCategoria.setEnabled(true);
+							 										}
+							 										
+							 										btnLimpiarFiltroClase.setEnabled(true);
 							 										btnFiltroProducto.setEnabled(false);
-//							 										btnFiltroClase.setEnabled(false);
 							 				break;
 							 				
-							 				case "categoria":		txtFiltroCategoria.setText(operador_simbolo+Lista)	;
+							 				case "categoria":		txtFiltroCategoria.setText(Lista)	;
+													 				if(Operador.equals("Igual")||Operador.equals("Esta en lista")){
+							 											btnFiltroFamilia.setEnabled(true);
+							 											btnLimpiarFiltroFamilia.setEnabled(true);
+							 										}
+													 				btnLimpiarFiltroCategoria.setEnabled(true);
 													 				btnFiltroProducto.setEnabled(false);
-							 										btnFiltroClase.setEnabled(false);
-//							 										btnFiltroClase.setEnabled(false);
 							 				break;
 							 				
 							 				
-							 				case "familia":			txtFiltroFamilia.setText(operador_simbolo+Lista)		;
+							 				case "familia":			txtFiltroFamilia.setText(Lista)		;
+													 				if(Operador.equals("Igual")||Operador.equals("Esta en lista")){
+							 											btnFiltroLinea.setEnabled(true);
+							 											btnLimpiarFiltroLinea.setEnabled(true);
+							 										}
+													 				btnLimpiarFiltroFamilia.setEnabled(true);
 													 				btnFiltroProducto.setEnabled(false);
-							 										btnFiltroClase.setEnabled(false);
-							 										btnFiltroCategoria.setEnabled(false);
-							 										
-							 										btnLimpiarFiltroCategoria.setEnabled(false);
 							 				break;
 							 				
-							 				case "linea_producto":	txtFiltroLinea.setText(operador_simbolo+Lista)		;
+							 				case "linea_producto":	txtFiltroLinea.setText(Lista)		;
+							 				
+							 										btnLimpiarFiltroLinea.setEnabled(true);
 													 				btnFiltroProducto.setEnabled(false);
-							 										btnFiltroClase.setEnabled(false);
-							 										btnFiltroCategoria.setEnabled(false);
-							 										btnFiltroFamilia.setEnabled(false);
-							 										
-							 										btnLimpiarFiltroCategoria.setEnabled(false);
-							 										btnLimpiarFiltroFamilia.setEnabled(false);
 	 										break;
 				 				}
 					 			dispose();
@@ -957,7 +1060,23 @@ public void filtroProductos(String cadena){
 				
 			   	public Object[][] getTablaFiltro(String operador, String nombre_de_tabla){
 //			   		String todos = "exec sp_select_filtro_empleados_con_cuadrante_asignado";
-					String todos = "select "+folio_columna+" as folio,nombre from "+nombre_de_tabla+" order by nombre";
+			   		String condicion = "";
+			   		
+			   		if(!Lista.equals("")){
+			   			
+			   			condicion = " where jerarquia "+Lista.replace("''","'");
+			   		}
+////			   		else{
+////			   			
+////			   		}
+//			   		if(nombre_de_tabla.equals("clases_productos")){
+//			   			condicion = " where folio = "+parametroGeneral;
+//			   		}
+			   		
+			   		
+					String todos = "select "+folio_columna+" as folio,nombre from "+nombre_de_tabla+condicion+" order by nombre";
+					
+					System.out.println(todos);
 					Statement s;
 					ResultSet rs;
 					try {
@@ -973,6 +1092,7 @@ public void filtroProductos(String cadena){
 							MatrizFiltro[i][2] = false;
 							i++;
 						}
+						Lista="";
 					} catch (SQLException e1) {
 						e1.printStackTrace();
 					}
