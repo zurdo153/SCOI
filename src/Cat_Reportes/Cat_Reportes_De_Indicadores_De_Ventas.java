c71b14613fd68bc285e13ca4fac497f4c6cac173
mabg1 <mabg161194mmn@hotmail.com>
2016-12-19 11:16:03 -0700

    19 12 2016 11 16 n

---
+++ src/Cat_Reportes/Cat_Reportes_De_Indicadores_De_Ventas.java
@@ -0,0 +1,211 @@
+package Cat_Reportes;
+
+import java.awt.Container;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+
+import Conexiones_SQL.BuscarSQL;
+import Conexiones_SQL.Generacion_Reportes;
+import Obj_Administracion_del_Sistema.Obj_Usuario;
+import Obj_Contabilidad.Obj_Indicadores;
+import Obj_Lista_de_Raya.Obj_Establecimiento;
+import Obj_Principal.JCButton;
+
+import com.toedter.calendar.JDateChooser;
+
+@SuppressWarnings("serial")
+public class Cat_Reportes_De_Indicadores_De_Ventas extends JFrame {
+	Container cont = getContentPane();
+	JLayeredPane panel = new JLayeredPane();
+	
+	JDateChooser c_inicio = new JDateChooser();
+	JDateChooser c_final = new JDateChooser();
+	
+	String operador[] = {"Selecciona Un Reporte","Reporte Indicador De Nivel De Surtido"};
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	JComboBox cmbConcepto = new JComboBox(operador);
+	
+	String establecimiento[] = new Obj_Establecimiento().Combo_Establecimiento();
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	JComboBox cmbEstablecimiento = new JComboBox(establecimiento);
+	
+	
+	JCButton btngenerar_reporte = new JCButton("Generar Reporte En Pantalla","hoja-de-calculo-icono-8865-32.png","Azul");
+	JCButton btngenerar_excel   = new JCButton("Generar Reporte En XLS ","xls-icon-3376-32px.png","Verde");
+	
+	JLabel JLBlinicio			= new JLabel(new ImageIcon("Imagen/iniciar-icono-4628-16.png") );
+	JLabel JLBfin				= new JLabel(new ImageIcon("Imagen/acabado-icono-7912-16.png") );
+	JLabel JLBestablecimiento	= new JLabel(new ImageIcon("Imagen/folder-home-home-icone-5663-16.png") );
+	JLabel JLBdepartamento		= new JLabel(new ImageIcon("Imagen/departamento-icono-5365-16.png") );
+	
+	public Cat_Reportes_De_Indicadores_De_Ventas(){
+		this.setSize(430,250);
+		this.setResizable(false);
+		
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		this.setTitle("Indicadore De Ventas");
+		this.setIconImage(Toolkit.getDefaultToolkit().getImage("Imagen/encuesta.png"));
+		this.panel.setBorder(BorderFactory.createTitledBorder("Seleccione  El Rango De Fechas,El Tipo de Reporte y Genere El Reporte "));
+
+		int x=20, y=25, width=100,height=20;
+		this.panel.add(new JLabel("Fecha Inicio:")).setBounds (x     ,y      ,width  ,height    );
+		this.panel.add(JLBlinicio).setBounds                  (x+=55 ,y      ,height ,height    );
+		this.panel.add(c_inicio).setBounds                    (x+=20 ,y      ,width  ,height    );
+		this.panel.add(new JLabel("Fecha Final:")).setBounds  (x+=130,y      ,width  ,height    );
+		this.panel.add(JLBfin).setBounds                      (x+=55 ,y      ,height ,height    );
+		this.panel.add(c_final).setBounds                     (x+=20 ,y      ,width  ,height    );
+		
+		x=20;width=380;
+		this.panel.add(cmbEstablecimiento).setBounds          (x     ,y+=35  ,width   ,height   );
+		this.panel.add(cmbConcepto).setBounds                 (x     ,y+=35  ,width   ,height   );
+		
+		x=70;width=300;
+		this.panel.add(btngenerar_reporte).setBounds          (x    ,y+=50   ,width   ,height*2 );
+//		this.panel.add(btngenerar_excel).setBounds            (x    ,y+=50   ,width   ,height*2 );
+
+		this.cont.add(panel);
+		
+		c_inicio.setDate( cargar_fechas(7));
+		c_final.setDate( cargar_fechas(0));
+
+		 btngenerar_reporte.addActionListener(opGenerar_reporte);
+	}
+	
+	public Date cargar_fechas(Integer dias){
+		Date date1 = null;
+				  try {
+					date1 = new SimpleDateFormat("dd/MM/yyyy").parse(new BuscarSQL().fecha(dias));
+				} catch (ParseException e) {
+					e.printStackTrace();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+		return date1;
+	};
+	
+	public String validar_fechas(){
+		String error = "";
+		String fechainicioNull = c_inicio.getDate()+"";
+		String fechafinalNull = c_final.getDate()+"";
+	    if(fechainicioNull.equals("null"))error+= "Fecha  inicio\n";
+		if(fechafinalNull.equals("null"))error+= "Fecha Final\n";
+		return error;
+	}
+	
+	ActionListener opGenerar_reporte = new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			String basedatos="2.26";
+			String vista_previa_reporte="no";
+			int vista_previa_de_ventana=0;
+			String comando= "";
+			String reporte = "";
+			
+			 if(cmbConcepto.getSelectedIndex()==0){
+			       JOptionPane.showMessageDialog(null,"Debe de Seleccionar Un Tipo De Reporte","Aviso!", JOptionPane.WARNING_MESSAGE,new ImageIcon("imagen/usuario-de-alerta-icono-4069-64.png"));
+			        cmbConcepto.requestFocus();
+			        cmbConcepto.showPopup();
+				    return;		
+			      }else{ 
+					  
+						if(validar_fechas().equals("")){
+							String fecha_inicio = new SimpleDateFormat("dd/MM/yyyy").format(c_inicio.getDate())+" 00:00:00";
+							  String fecha_final  = new SimpleDateFormat("dd/MM/yyyy").format(c_final.getDate())+"  23:59:00";
+							  SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm"); 
+							  Date fecha1 = sdf.parse(fecha_inicio , new ParsePosition(0));
+							  Date fecha2 = sdf.parse(fecha_final , new ParsePosition(0));
+							  
+							  if(fecha1.before(fecha2)){
+								  String concepto=cmbConcepto.getSelectedItem().toString().trim();
+								  
+								  if(concepto.equals("Reporte Indicador De Nivel De Surtido")){
+									  Obj_Indicadores indicador = new Obj_Indicadores().buscar("Nivel De Surtido");
+										comando=indicador.getProcedimiento_almacenado()+" '"+fecha_inicio+"','"+fecha_final+"','"+new Obj_Usuario().getNombre_completo()+"',''";
+										reporte =indicador.getReporte();
+								   }
+								  
+								  if(concepto.equals("Reporte Indicador De Nivel De Servicio")){
+									  Obj_Indicadores indicador = new Obj_Indicadores().buscar("Nivel De Servicio");
+										comando=indicador.getProcedimiento_almacenado()+" '"+fecha_inicio+"','"+fecha_final+"'";
+										reporte =indicador.getReporte();
+								   }
+								  
+								  if(concepto.equals("Reporte De Productos Negados Por Establecimientos")){
+									    comando="exec sp_select_productos_negados_por_establecimiento '"+fecha_inicio+"','"+fecha_final+"'";
+										reporte = "Obj_Reporte_Productos_Negados_Por_Establecimiento.jrxml";
+								   }
+								  
+								  if(concepto.equals("Reporte De Total De Productos Negados Con Localizacion")){
+								        comando="exec sp_select_total_de_productos_negados '"+fecha_inicio+"','"+fecha_final+"'";
+									    reporte = "Obj_Reporte_De_Total_De_Productos_Negados.jrxml";
+								  }
+
+							  }else{
+						        JOptionPane.showMessageDialog(null,"El Rango de Fechas Esta Invertido","Aviso!", JOptionPane.WARNING_MESSAGE,new ImageIcon("Imagen/usuario-de-alerta-icono-4069-64.png"));
+							     return;
+							  }
+							
+						}else{
+						  JOptionPane.showMessageDialog(null, "Los Siguientes Campos Estan Vacios y Se Necesitan Para La Consulta:\n "+validar_fechas(),"Aviso", JOptionPane.ERROR_MESSAGE,new ImageIcon("Imagen/usuario-de-alerta-icono-4069-64.png"));
+			               return;
+						} 
+		       }
+			    new Generacion_Reportes().Reporte(reporte, comando, basedatos, vista_previa_reporte,vista_previa_de_ventana);
+			   return;
+		}
+	};
+	
+//	ActionListener opGenerar_XLS = new ActionListener() {
+//		public void actionPerformed(ActionEvent e) {
+//			String basedatos="2.200";
+//			String vista_previa_reporte="no";
+//			int vista_previa_de_ventana=0;
+//			String comando= "";
+//			String reporte = "";
+//			String fecha_guardado="";
+//				try {fecha_guardado=new BuscarSQL().fecha_guardado(); } catch (SQLException e1) {e1.printStackTrace();}
+//			
+//			if(cmbEstablecimiento.getSelectedIndex()==0){
+//			   JOptionPane.showMessageDialog(null,"Debe de Seleccionar Un Establecimiento: ","Aviso!", JOptionPane.WARNING_MESSAGE,new ImageIcon("imagen/usuario-de-alerta-icono-4069-64.png"));
+//			      cmbEstablecimiento.requestFocus();
+//				  cmbEstablecimiento.showPopup();
+//			   	 return;		
+//			}else{
+//				  if(cmbConcepto.getSelectedIndex()==0){
+//			    	 JOptionPane.showMessageDialog(null,"Debe de Seleccionar Un Tipo De Reporte","Aviso!", JOptionPane.WARNING_MESSAGE,new ImageIcon("imagen/usuario-de-alerta-icono-4069-64.png"));
+//			    	    cmbConcepto.requestFocus();
+//			    	    cmbConcepto.showPopup();
+//				     return;		
+//				  }else{ 
+//				 	 comando = new Cat_Comandos().Comandos_Maximos_y_Minimos(cmbConcepto.getSelectedItem().toString().trim(), cmbEstablecimiento.getSelectedItem().toString().trim());
+//   				     reporte="Obj_Reporte_De_Pedido_Sugerido_Maximos_y_Minimos.jrxml";
+//		    }
+//				  new Generacion_Reportes().Reporte_Guardado(reporte, comando, basedatos, vista_previa_reporte,vista_previa_de_ventana,"xls",cmbConcepto.getSelectedItem().toString().trim()+" "+cmbEstablecimiento.getSelectedItem().toString().trim()+"_"+fecha_guardado);
+//			   return;
+//			}
+//		}
+//	};
+	
+	public static void main(String args[]){
+		try{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			new Cat_Reportes_De_Indicadores_De_Ventas().setVisible(true);
+		}catch(Exception e){	}
+	}
+
+}
---
+++ src/Obj_Compras/Obj_Ubicaciones_De_Productos.java
@@ -0,0 +1,113 @@
+package Obj_Compras;
+
+import java.sql.SQLException;
+
+import Conexiones_SQL.BuscarSQL;
+
+public class Obj_Ubicaciones_De_Productos {
+	String Cod_Prod;
+	String Descripcion_Prod;
+	String Localizacion;
+	String Clase_De_Producto;
+	String Categoria;
+	String Familia;
+	String Area;
+	
+	double Ultimo_Costo;
+	double Costo_Promedio;
+	double Existencia_Cedis;
+	double Existencia_Total;
+	double Cantidad_Negada_Ultimos_7_dias;
+	double precio_de_venta;
+	
+	public String getCod_Prod() {
+		return Cod_Prod;
+	}
+	public void setCod_Prod(String cod_Prod) {
+		Cod_Prod = cod_Prod;
+	}
+	public String getDescripcion_Prod() {
+		return Descripcion_Prod;
+	}
+	public void setDescripcion_Prod(String descripcion_Prod) {
+		Descripcion_Prod = descripcion_Prod;
+	}
+	public String getLocalizacion() {
+		return Localizacion;
+	}
+	public void setLocalizacion(String localizacion) {
+		Localizacion = localizacion;
+	}
+	public String getClase_De_Producto() {
+		return Clase_De_Producto;
+	}
+	public void setClase_De_Producto(String clase_De_Producto) {
+		Clase_De_Producto = clase_De_Producto;
+	}
+	public String getCategoria() {
+		return Categoria;
+	}
+	public void setCategoria(String categoria) {
+		Categoria = categoria;
+	}
+	public String getFamilia() {
+		return Familia;
+	}
+	public void setFamilia(String familia) {
+		Familia = familia;
+	}
+	public String getArea() {
+		return Area;
+	}
+	public void setArea(String area) {
+		Area = area;
+	}
+	public double getUltimo_Costo() {
+		return Ultimo_Costo;
+	}
+	public void setUltimo_Costo(double ultimo_Costo) {
+		Ultimo_Costo = ultimo_Costo;
+	}
+	public double getCosto_Promedio() {
+		return Costo_Promedio;
+	}
+	public void setCosto_Promedio(double costo_Promedio) {
+		Costo_Promedio = costo_Promedio;
+	}
+	public double getExistencia_Cedis() {
+		return Existencia_Cedis;
+	}
+	public void setExistencia_Cedis(double existencia_Cedis) {
+		Existencia_Cedis = existencia_Cedis;
+	}
+	public double getExistencia_Total() {
+		return Existencia_Total;
+	}
+	public void setExistencia_Total(double existencia_Total) {
+		Existencia_Total = existencia_Total;
+	}
+	public double getCantidad_Negada_Ultimos_7_dias() {
+		return Cantidad_Negada_Ultimos_7_dias;
+	}
+	public void setCantidad_Negada_Ultimos_7_dias(double cantidad_Negada_Ultimos_7_dias) {
+		Cantidad_Negada_Ultimos_7_dias = cantidad_Negada_Ultimos_7_dias;
+	}
+	public double getPrecio_de_venta() {
+		return precio_de_venta;
+	}
+	public void setPrecio_de_venta(double precio_de_venta) {
+		this.precio_de_venta = precio_de_venta;
+	}
+
+	public Obj_Ubicaciones_De_Productos buscardatos_producto(String cod_prod, String establecimiento){ 
+		try {
+			return new BuscarSQL().datos_producto_ubicacion(cod_prod,establecimiento);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	return null; 
+	}
+	
+}
+
+
