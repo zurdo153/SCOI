df7fb0f8973b1631c797e063c3f6fc8d31919594
zurdo153 <zurdo153@hotmail.com>
2018-12-31 13:13:11 -0700

    subir cambios en catalogo de surtudo de orden de compra interna 31-12-2018 1.12

--- src/Cat_Contabilidad/Cat_Conciliar_Movimientos_Temporales.java
+++ src/Cat_Contabilidad/Cat_Conciliar_Movimientos_Temporales.java
@@ -5,9 +5,6 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.text.DecimalFormat;
 
 import javax.swing.BorderFactory;
